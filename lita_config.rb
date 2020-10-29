b152052b8f7d904f0adeb0c8546e4c573ae9c77a
white3 <1070072593@qq.com>
2020-10-30 00:27:35 +0800

    lalalalala

--- lita_config.rb
+++ lita_config.rb
@@ -33,21 +33,21 @@ Lita.configure do |config|
   ## documentation for options.
   # aconfig.handlers.doubler =
   # config.handlers.some_handler.some_config_key = "value"
-  # config.robot.adapter = :slack
-  # config.adapters.slack.token = "xoxb-1458563476386-1451835816102-M0Phs506NiEtYLGRTZLJesah"
+  config.robot.adapter = :slack
+  config.adapters.slack.token = "xoxb-1458563476386-1451835816102-1oxxXtBhYFV5zDhdJsNJLk1M"
 
 # The adapter you want to connect with. Make sure you've added the
 #
 # appropriate gem to the Gemfile.
 #
 # heroku uses a RACK_ENV of 'production' by default
-  if ENV['BACK_ENV']=='production'
-    config.robot.adapter =:slack
-    config.redis[:url]=ENV.fetch('REDIS_URL')
-  else
-    config.robot.adapter=:shell
-  end
+  #if ENV['BACK_ENV']=='production'
+  #  config.robot.adapter =:slack
+  #  config.redis[:url]=ENV.fetch('REDIS_URL')
+  #else
+  #  config.robot.adapter=:shell
+  #end
 
 # slack adapter demands a value even in dev when wa aren;t using it...
-  config.adapters.slack.token = ENV.fetch('SLACK_TOKEN','xoxb-1458563476386-1451835816102-1oxxXtBhYFV5zDhdJsNJLk1M')
+  #config.adapters.slack.token = ENV.fetch('SLACK_TOKEN','xoxb-1458563476386-1451835816102-1oxxXtBhYFV5zDhdJsNJLk1M')
 end
