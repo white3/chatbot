752201557a0bb1f07e8c279534c0f73e925e7dfd
white3 <1070072593@qq.com>
2020-10-31 23:34:22 +0800

    lalalalala

--- lita_config.rb
+++ lita_config.rb
@@ -49,5 +49,5 @@ Lita.configure do |config|
   end
 
 # slack adapter demands a value even in dev when wa aren;t using it...
-  config.adapters.slack.token = ENV.fetch('SLACK_TOKEN','xoxb-1458563476386-1451835816102-GG68RpykOdJdT6QTt40sYQtz')
+  config.adapters.slack.token = ENV.fetch('SLACK_TOKEN','xoxb-1458563476386-1451835816102-oQWQueyOMOPNKzUr2p1k3i77')
 end
